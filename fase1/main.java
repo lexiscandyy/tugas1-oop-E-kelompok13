78a6af50700d58fff7fa1cc92a31e873ce5bcca0
ra** in**** <rama.id0174@gmail.com>
2026-04-10 13:32:50 +0800

    tes

--- fase1/main.java
+++ fase1/main.java
@@ -1,1 +1,10 @@
+import java.util.*;
+import java.io.*;
 
+public class main{
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] arg){
+        System.out.println("adawdad");
+    }
+}
