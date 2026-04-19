02f89a6d7330be8d3691361e05f5384db25c8670
ra** in**** <rama.id0174@gmail.com>
2026-04-19 22:10:27 +0800

    a

--- fase1/Utils.java
+++ fase1/Utils.java
@@ -4,18 +4,12 @@ public class Utils{
     static Scanner sc = new Scanner(System.in);
     public int ScanInt(){
         String str = sc.next();
-        boolean ok = true;
-
         for(int i = 0; i < str.length();i++){
-            if(str.charAt(i) < '0' || str.charAt(i) > '9'){
-                ok = false;
-                break;
-            }
+            if(str.charAt(i) < '0' || str.charAt(i) > '9') return -1;
         }
 
         if(str.length() > 10) return -1;
-        if(ok) return Integer.parseInt(str);
-        return -1;
+        return Integer.parseInt(str);
     }
 
     public String ScanString(){
