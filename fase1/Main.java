f9c7b3b837c7bd272894927328c4f51510236764
ra** in**** <rama.id0174@gmail.com>
2026-04-19 23:20:10 +0800

    a

--- fase1/Main.java
+++ fase1/Main.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class Main{
     public static int cur = 0;
+    static ArrayList<String[]> kamar = new ArrayList<>();
 
     public static void main(String[] arg){
         String[] listMenu = {
@@ -15,7 +16,8 @@ public class Main{
                 "4. Kembali\n",
 
                 "1. Edit\n" +
-                "2. Kembali\n",
+                "2. Tambah Kamar\n" +
+                "3. Kembali\n",
         };
 
         while (true) {
@@ -31,24 +33,52 @@ public class Main{
                 if(input == 2) tambahPenghuni();
                 if(input == 3) riwayatCatatanPembayaran();
             }else if(cur == 2){
-                if(input == 2) cur--;
+                if(input == 3) cur--;
                 if(input == 1) editKamar();
+                if(input == 2) tambahKamar();
             }
         }
     }
 
     public static void listKamar(){
-        System.out.println("1. Kamar 1, fasilitas .. .......\n" +
-                "1. Kamar 1, fasilitas .. .......\n" +
-                "1. Kamar 1, fasilitas .. .......\n" +
-                "1. Kamar 1, fasilitas .. .......\n" +
-                "1. Kamar 1, fasilitas .. .......\n" +
-                "1. Kamar 1, fasilitas .. .......\n");
+//        System.out.println("1. Kamar 1, fasilitas .. .......\n" +
+//                "1. Kamar 1, fasilitas .. .......\n" +
+//                "1. Kamar 1, fasilitas .. .......\n" +
+//                "1. Kamar 1, fasilitas .. .......\n" +
+//                "1. Kamar 1, fasilitas .. .......\n" +
+//                "1. Kamar 1, fasilitas .. .......\n");
+        int nomor = new Integer(1);
+        for(String[] data : kamar){
+            System.out.println(nomor++ + ". Fasilitas: " + data[0] + "\n\tStatus : " + data[1]);
+        }
         cur++;
     }
 
     public static void editKamar(){
+        System.out.println("Masukkan nomor kamar yang ingin di edit");
+        while(true){
+            if(kamar.size() == 0) return;
+            int input = new Utils().ScanInt();
+            if(input > kamar.size() || input <= 0) continue;
+            input--;
+            System.out.println("Ketik fasilitas atau ketik -1 untuk tidak melakukan perubahan");
+            String fasilitas = new Utils().ScanString(1000);
+            System.out.println("Masukkan status: terisi atau belum terisi");
+            String ada = new Utils().ScanString(100);
 
+            while(fasilitas == null) fasilitas = new Utils().ScanString(1000);
+            while(ada == null) ada = new Utils().ScanString(100);
+
+            if(fasilitas != "-1") kamar.get(input)[0] = fasilitas;
+            kamar.get(input)[1] = ada;
+            System.out.println("data berhasil di edit");
+            return;
+        }
+    }
+
+    public static void tambahKamar(){
+        kamar.add(new String[]{"Fasilitas", "Belum Terisi"});
+        System.out.println("Berhasil menambahkan 1 kamar");
     }
 
     public static void tambahPenghuni(){
--- fase1/Utils.java
+++ fase1/Utils.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Utils{
     static Scanner sc = new Scanner(System.in);
     public int ScanInt(){
-        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println("tekan enter");
+        sc.nextLine();
         for(int i = 0; i < str.length();i++){
             if(str.charAt(i) < '0' || str.charAt(i) > '9') return -1;
         }
@@ -13,7 +15,9 @@ public class Utils{
     }
 
     public String ScanString(int lenLimit){
-        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println("tekan enter");
+        sc.nextLine();
         return (str.length() > lenLimit ? null : str);
     }
 }
